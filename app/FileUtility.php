6377efaaff816ed00bffa53967eae2407529e37e
Romack Natividad <romacknatividad@gmail.com>
2022-03-18 18:39:36 +0800

    Wrap opening of a file in a try catch block

--- app/FileUtility.php
+++ app/FileUtility.php
@@ -4,24 +4,29 @@ class FileUtility
 {
     public static function openCSV($csv_file)
     {
-        $file = fopen($csv_file, 'r');
         $data = [];
         $headers = [];
         $index = 0;
-        while($row = fgetcsv($file)) {
-            if ($index == 0) {
-                $headers = $row;
-                $index++;
-                continue;
-            }
+        try {
+            $file = fopen($csv_file, 'r');
+            while($row = fgetcsv($file)) {
+                if ($index == 0) {
+                    $headers = $row;
+                    $index++;
+                    continue;
+                }
 
-            $csv_data = [];
-            for ($i = 0; $i < count($row); $i++) {
-                $csv_data[$headers[$i]] = $row[$i];
+                $csv_data = [];
+                for ($i = 0; $i < count($row); $i++) {
+                    $csv_data[$headers[$i]] = $row[$i];
+                }
+                array_push($data, $csv_data);
+                $index++;
             }
-            array_push($data, $csv_data);
-            $index++;
+        } catch (Exception $e) {
+            error_log($e->getMessage());
         }
+        
         return $data;
     }
 
